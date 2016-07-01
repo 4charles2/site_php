096e1b6fffc0d2e96abf1f9f43c24d178090efbf
4charles2 <ctognol@laposte.net>
2016-07-01 14:15:23 +0200

    ajout fichier test sur branch test

--- pages/git/git.php
+++ pages/git/git.php
@@ -6,6 +6,7 @@
 		<ul>
 			<li><a href="#compte">Création d'un compte</a></li>
 			<li><a href="#exclude">Exclure des dossiers ou fichiers depuis le fichier de configuration</a></li>
+			<li><a href="#branch">Git branch creer, lister les branches, fusionner, changer de branche</a></li>
 			<li><a href="#command_basic">Les commandes basic de git</a></li>
 		</ul>
 	</article>
@@ -17,6 +18,16 @@
 		<p>On peut exclure des fichiers grâce au fichier .gitignore ou exclude qui se trouve dans le dossier .git/info/
 		grace à la variable core.excludesFile ou dans ~/.gitconfig</p>
 		<p>Soit en indiquant le chemin soit avec des expressions rationnelle</p>
+		<p>Voici un exemple de fichier qui ne exclu les fichiers qui commence par une lettre quelconque suivi d'un o ou a puis les fichiers qui se termine par un ~ et le fichier bdd.connexion.php<pre><code>
+&gt;# git ls-files --others --exclude-from=.git/info/exclude
+&gt;# Lines that start with '#' are comments.
+&gt;# For a project mostly in C, the following would be a good set of
+&gt;# exclude patterns (uncomment them if you want to use them):
+&gt;*.[oa]
+&gt;*~
+&gt;bdd/connexion.php
+		</code></pre></p>
+		<p>Cette exclution et valable uniquement dans le dossier courant de git si on veut que cela s'applique à tous les dépôts de l'utilisateur en cours alors aller modifier le fichier dans ~/.gitconfig</p>
 	</article>
 </section>
 
@@ -24,6 +35,16 @@
 	<h1><u>Créer un compte afin de pouvoir utiliser github</u></h1>
 	<article>
 		<p>Pour utiliser git il vous faut un compte sur le site internet github.com afin d'obtenir un espace de stockage gratuitement si vous voulez que votre code soit public ou payant afin de le rendre invisible au public</p>
+	</article>
+</section>
+
+<section id="branch">
+	<h1><u>Creer, Lister, fusionner, changer de branche avec git</u></h1>
+	<article>
+		<p><u>Pour créer une branche et y basculer tout de suite :</u></p>
+		<p>La commande <code>&gt;git checkout -b nomDeBranche</code></p>
+		<p>Cette commande est un raccourci de <code>&gt;git branch nomDeBranch</code> qui créer une branche et 
+			<code>&gt;git checkout nomdeBranch</code> qui sélectionne le nom de la branch fournie en argument</p>
 	</article>
 </section>
 
