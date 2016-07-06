12f8eebca72c64f49b3103efb9712796c8e41b89
4charles2 <ctognol@laposte.net>
2016-07-06 11:42:35 +0200

    completion

--- pages/html/html.php
+++ pages/html/html.php
@@ -8,6 +8,7 @@
 					<li><a href="#entities">Les entities html lettres avec accents ou symbÃ´le comme &amp;</a></li>
 					<li><a href="#encode">DÃ©finir l'encodage du fichier avec la balise meta</a></li>
 					<li><a href="#figure">Les figures et figcaption pour attribuer une legende</a></li>
+					<li><a href="#special_balise">Les balises de presentation spÃ©ciale comme blockquote</a></li>
 				</ul>
             </article>
         </section>
@@ -85,40 +86,40 @@
 <section id="figure">
 	<h1><u>Utilisation de figure et figcaption</u></h1>
 	<article>
-		<p>Pour attribuer une legende à une image on utilise les balises figure et figcaption</p>
-		<p>La balise figure pour former un bloc des elements auquel on souhaite définir une legende</p>
+		<p>Pour attribuer une legende ? une image on utilise les balises figure et figcaption</p>
+		<p>La balise figure pour former un bloc des elements auquel on souhaite d?finir une legende</p>
 		<p>Pour attribuer la legende c'est la balise figcaption qui n'est pas obligatoire</p>
-		<p>Cela peut servir pour diffèrents élements comme les img mais aussi les blocs de code ou encore des schéma et autre ...</p>
-		<p>La diffèrence entre aside et figure : </p>
-		<p>Ces deux élements sont des unités de contenu mais n'ont pas le même rôle sémantique. Tandis qu'&lt;aside&gt; n'est pas essentiel pour la compréhension de la plage (il ne doit être qu'un apport tangentiel), &lt;figure&gt; est lié à celle-ci : il transmet un contenu pertinent, lié au contenu principal, comme le serait une image classique.</p>
+		<p>Cela peut servir pour diff?rents ?lements comme les img mais aussi les blocs de code ou encore des sch?ma et autre ...</p>
+		<p>La diff?rence entre aside et figure : </p>
+		<p>Ces deux ?lements sont des unit?s de contenu mais n'ont pas le m?me r?le s?mantique. Tandis qu'&lt;aside&gt; n'est pas essentiel pour la compr?hension de la plage (il ne doit ?tre qu'un apport tangentiel), &lt;figure&gt; est li? ? celle-ci : il transmet un contenu pertinent, li? au contenu principal, comme le serait une image classique.</p>
 		<ul>
-			<h3>Nous pouvons donc résumer la chose comme suit : </h3>
-			<li>&lt;aside&gt; : son contenu est périphérique à la page, et son absence ne doit pas gêner la compréhension de celle-ci.</li>
-			<li>&lt;figure&gt; : son contenu contribue à donner du sens et de la compréhension à la page, et son emplacement n'est pas déterminant.</li>
+			<h3>Nous pouvons donc r?sumer la chose comme suit : </h3>
+			<li>&lt;aside&gt; : son contenu est p?riph?rique ? la page, et son absence ne doit pas g?ner la compr?hension de celle-ci.</li>
+			<li>&lt;figure&gt; : son contenu contribue ? donner du sens et de la compr?hension ? la page, et son emplacement n'est pas d?terminant.</li>
 		</ul>
 		<table>
 			<caption>&lt;figure&gt;</caption>
 			<thead>
 				<tr>
-					<th>Propriété</th>
-					<th>Détails</th>
+					<th>Propri?t?</th>
+					<th>D?tails</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr>
-					<td>Modèles de contenu autorisés</td>
-					<td>Un élément &lt;figcaption&gt; optionnel, suivi par du contenu de flux.</td>
+					<td>Mod?les de contenu autoris?s</td>
+					<td>Un ?l?ment &lt;figcaption&gt; optionnel, suivi par du contenu de flux.</td>
 				</tr>
 				<tr>
-					<td>Parents autorisés</td>
-					<td>Tout élément pouvant contenir des éléments du flux</td>
+					<td>Parents autoris?s</td>
+					<td>Tout ?l?ment pouvant contenir des ?l?ments du flux</td>
 				</tr>
 				<tr>
 					<td>Omission de balise</td>
 					<td>Les balises ouvrantes et fermantes sont obligatoires</td>
 				</tr>
 				<tr>
-					<td>Style par défaut</td>
+					<td>Style par d?faut</td>
 					<td>figcaption { display:bloc; margin:1em 40px; }</td>
 				</tr>
 			</tbody>
@@ -127,17 +128,17 @@
 			<caption>&lt;figcaption&gt;</caption>
 			<thead>
 				<tr>
-					<th>Propiété</th>
-					<th>Détails</th>
+					<th>Propi?t?</th>
+					<th>D?tails</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr>
-					<td>Modèles de contenu autorisés</td>
+					<td>Mod?les de contenu autoris?s</td>
 					<td>Contenu de flux</td>
 				</tr>
 				<tr>
-					<td>Parents autorisés</td>
+					<td>Parents autoris?s</td>
 					<td>&lt;figure&gt;</td>
 				</tr>
 				<tr>
@@ -145,10 +146,22 @@
 					<td>Les balises ouvrante et fermantes sont obligatoires</td>
 				</tr>
 				<tr>
-					<td>Style par défaut</td>
+					<td>Style par d?faut</td>
 					<td>figcaption {display:block;}</td>
 				</tr>
 			</tbody>
 		</table>
+	</article>
+</section>
+<section id="special_balise">
+	<h1><u>Les balises de prÃ©sentation du texte comme blockquote </u></h1>
+	<article>
+		<p>&lt;abbr&gt;	Defines an abbreviation or acronym definie une abbreviation ou un acronyme avec une infobulle</p>
+		<p>&lt;address&gt; Defines contact information for the author/owner of a document Definie les informations pour contacter l'auteur ou le propriÃ©taire du document </p>
+		<p>&lt;bdo&gt;	Defines the text direction Definies la direction du text comme par exemple &lt;bdo dir="rtl"&gt;texte&lt;bdo&gt; affichera : <bdo dir="rtl">texte</bdo></p>
+		<p>&lt;blockquote&gt;	Defines a section that is quoted from another source definie une citation en bloc qui viens d'une autre source qui peut Ãªtre definie par l'atribut cite exemple &lt;blockquote cite="http://www.openclassroom.com"&gt;Toutes les connaissances rÃ©sumÃ© sur ce site provienne du site internet openclassroom.com&lt;blockquote&gt; Ce qui donne comme rÃ©sultat : </p>
+		<blockquote cite="http://www.openclassroom.com">Toutes les connaissances rÃ©sumÃ© sur ce site provienne du site internet openclassroom.com</blockquote>
+		<p>&lt;cite&gt;	Defines the title of a work definie le titre d'un travail</p>
+		<p>&lt;q&gt;	Defines a short inline quotation definie une citation sur une ligne "citation " </p>
 	</article>
 </section>
